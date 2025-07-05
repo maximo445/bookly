e62bac58154f8175f0fdcecd258cfebd917649b9
Maximo Brito <britofilmmaker@gmail.com>
2025-07-05 13:43:24 -0400

    preparing for dployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: "/bookly",
 });
